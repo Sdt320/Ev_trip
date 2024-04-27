11d52d65dec1d8782ed2d33a312cdd5002f6c801
Dharshini T <dharshinit0303@gmail.com>
2024-04-27 16:24:20 +0530

    Authentication with clerk

--- App.js
+++ App.js
@@ -4,32 +4,64 @@ import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
 import { useCallback } from 'react';
 import LoginScreen from './App/Screen/LoginScreen/LoginScreen';
+import * as SecureStore from "expo-secure-store";
+import { ClerkProvider, SignedIn, SignedOut  } from "@clerk/clerk-expo";
 
 
 SplashScreen.preventAutoHideAsync();
+
+
+const tokenCache = {
+  async getToken(key) {
+    try {
+      return SecureStore.getItemAsync(key);
+    } catch (err) {
+      return null;
+    }
+  },
+  async saveToken(key, value) {
+    try {
+      return SecureStore.setItemAsync(key, value);
+    } catch (err) {
+      return;
+    }
+  },
+};
 export default function App() {
 
-  const [fontsLoaded, fontError] = useFonts({
+  const [fontsLoaded] = useFonts({
     'outfit': require('./assets/fonts/Outfit-Regular.ttf'),
     'outfit-medium': require('./assets/fonts/Outfit-SemiBold.ttf'),
     'outfit-bold': require('./assets/fonts/Outfit-Bold.ttf'),
   });
 
   const onLayoutRootView = useCallback(async () => {
-    if (fontsLoaded || fontError) {
+    if (fontsLoaded) {
       await SplashScreen.hideAsync();
     }
-  }, [fontsLoaded, fontError]);
+  }, [fontsLoaded]);
 
-  if (!fontsLoaded && !fontError) {
+  if (!fontsLoaded) {
     return null;
   }
 
   return (
+    <ClerkProvider 
+    tokenCache={tokenCache}
+    publishableKey={'pk_test_dXB3YXJkLXJhY2VyLTcuY2xlcmsuYWNjb3VudHMuZGV2JA'}>
+
     <View style={styles.container} onLayout={onLayoutRootView}>
-      <LoginScreen/>
+      <SignedIn>
+          <Text>You are Signed in</Text>
+      </SignedIn>
+      <SignedOut>
+        <LoginScreen/>
+      </SignedOut>
+      
       <StatusBar style="auto" />
     </View>
+    </ClerkProvider>
+
   );
 }
 
--- App/Screen/LoginScreen/LoginScreen.jsx
+++ App/Screen/LoginScreen/LoginScreen.jsx
@@ -1,8 +1,31 @@
 import { View, Text, Image, StyleSheet, Touchable, TouchableOpacity } from 'react-native'
 import React from 'react'
 import Colors from '../../Utils/Colors'
+import * as WebBrowser from "expo-web-browser"
+import { useWarmUpBrowser } from '../../../hooks/useWarmUpBrowser';
+import { useOAuth } from '@clerk/clerk-expo';
 
+
+WebBrowser.maybeCompleteAuthSession();
 export default function LoginScreen() {
+    useWarmUpBrowser();
+
+  const { startOAuthFlow } = useOAuth({ strategy: "oauth_google" });
+  const onPress=async()=>{
+    try {
+        const { createdSessionId, signIn, signUp, setActive } =
+          await startOAuthFlow();
+  
+        if (createdSessionId) {
+          setActive({ session: createdSessionId });
+        } else {
+          // Use signIn or signUp for next steps such as MFA
+        }
+      } catch (err) {
+        console.error("OAuth error", err);
+      }
+  }
+
   return (
     <View style={{
         display:'flex',
@@ -21,7 +44,7 @@ export default function LoginScreen() {
             <Text style={styles.desc}>Find Ev charging station near you, plan trip and so much more in just one click</Text>
 
             <TouchableOpacity style={styles.button}
-            onPress={()=>console.log("Button Click")}>
+            onPress={onPress}>
                 
                 <Text style={{
                     color:Colors.WHITE,
