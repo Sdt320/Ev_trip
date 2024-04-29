71673cdf7bfef2953aa7d037fe4a411a77eb3ddd
Dharshini T <dharshinit0303@gmail.com>
2024-04-29 20:19:11 +0530

    google map api fetch

--- App/Screen/HomeScreen/HomeScreen.jsx
+++ App/Screen/HomeScreen/HomeScreen.jsx
@@ -1,10 +1,37 @@
 import { View, Text, StyleSheet } from 'react-native'
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
 import AppMapView from './AppMapView'
 import Header from './Header'
 import SearchBar from './SearchBar'
+import { UserLocationContext } from '../../Context/UserLocationContex'
+import GlobalApi from '../../Utils/GlobalApi'
 
 export default function HomeScreen() {
+
+  const {location,setLocation}=useContext(UserLocationContext);
+
+  useEffect(()=>{
+    location&&GetNearByPlace();
+
+  },[location])
+  const GetNearByPlace=()=>{
+    const data={
+      "includedTypes": ["electric_vehicle_charging_station"],
+      "maxResultCount": 10,
+      "locationRestriction": {
+        "circle": {
+          "center": {
+            "latitude": location?.latitude,
+            "longitude": location?.longitude},
+          "radius": 5000.0
+        }
+      }
+    }
+
+    GlobalApi.NewNearByPlace(data).then(resp=>{
+      console.log(JSON.stringify(resp.data));
+    })
+  }
   return (
     <View>
       <View style={styles.headerContainer}>
---
+++ App/Utils/GlobalApi.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+
+const BASE_URL="https://places.googleapis.com/v1/places:searchNearby";
+const API_KEY="AIzaSyB78zNvSMWjak4MQLvKR1RbDQHbjuIc5AE";
+
+const config={
+    headers:{
+        'Content-Type':'application/json',
+        "X-Goog-Api-Key":API_KEY,
+        "X-Goog-FieldMask": ["places.displayName",
+            'places.formattedAddress',
+            'places.location',
+            'places.evChargeOptions',
+            'places.photos']
+    }
+}
+
+const NewNearByPlace=(data)=>axios.post(BASE_URL,data,config);
+
+export default{
+    NewNearByPlace
+}
